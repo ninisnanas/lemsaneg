337206413d61f4dc9bcab0528937e3069b294b37
Aditya Try Anggoro <try.aditya@gmail.com>
2015-02-23 23:00:45 +0700

    fix implementation for SAC & BIC.

---
+++ TestSuite/src/bic/BIC.java
@@ -0,0 +1,224 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bic;
+
+import blockcipher.BlockCipherFactory;
+import blockcipher.CipherType;
+import java.util.ArrayList;
+import javax.xml.bind.DatatypeConverter;
+import java.util.BitSet;
+import java.util.List;
+
+/**
+ *
+ * @author ASUS
+ */
+public class BIC {
+    private int numOfSample;
+    private BitSet randomInput;
+    private BitSet baseOutput;
+    private BitSet flipInput;
+    private BitSet xorResult;
+    private BlockCipherFactory cipherFactory;
+    private int N = 16;
+    private byte[] plainText;
+    
+    private Data[][] arrayOfRawData = new Data[N][N];
+    private double[][] arrOfCoeffData = new double[N][N];
+    
+    public BIC(int numOfSample, String keyStr) {
+        this.numOfSample = numOfSample;
+        byte[] key_byte = toByteArray(transformFromHex(keyStr));
+        cipherFactory = new BlockCipherFactory(key_byte);
+    }
+    
+    public BIC() {
+        this(128, "0123456789ABCDEFFEDCBA9876543210");
+    }
+    
+    public double runTest() {
+        String inputStr;
+        for(int i = 0; i < numOfSample; i++) {
+            System.out.println(i);
+            inputStr = generateRandomInput();
+            randomInput = transformFromHex(inputStr);
+            
+            plainText = toByteArray(randomInput);
+            baseOutput = BitSet.valueOf(cipherFactory.getEncrypt(plainText, CipherType.AES));
+            
+            for(int j = 0; j < N; j++) {
+                flipInput = BitSet.valueOf(toByteArray(randomInput));
+                flipInput.flip(j);
+
+                plainText = toByteArray(flipInput);
+                xorResult = BitSet.valueOf(cipherFactory.getEncrypt(plainText, CipherType.AES));
+                xorResult.xor(baseOutput);
+                
+                for(int k = 0; k < N; k++) {
+                    for(int l = 0; l < N; l++) {
+                        if(k != l) {
+                            int x = xorResult.get(k) ? 1 : 0;
+                            int y = xorResult.get(l) ? 1 : 0;
+
+                            if(arrayOfRawData[k][l] == null)
+                                arrayOfRawData[k][l] = new Data();
+
+                            arrayOfRawData[k][l].add(x, y);
+                        }
+                    }
+                }
+           }
+        }
+        
+        double maxCoef = 0.0;
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(i != j) {
+                    Integer[] xData = arrayOfRawData[i][j].getXData();
+                    Integer[] yData = arrayOfRawData[i][j].getYData();
+
+                    double coeficient = getCorrelationCoefficient(xData, yData);
+                    arrOfCoeffData[i][j] = coeficient * coeficient;
+                    if(arrOfCoeffData[i][j] > maxCoef) {
+                        maxCoef = arrOfCoeffData[i][j];
+                    }
+                }
+            }
+        }
+        
+        return maxCoef;
+    }
+    
+    public void printArray(Integer[] arr) {
+        for(Integer i : arr) {
+            System.out.print("" + i + " ");
+        }
+        System.out.println();
+    }
+    
+    public static void main(String ar[]) {
+        BIC bic = new BIC();
+        double result = bic.runTest();
+        
+        System.out.println(result);
+    }
+    
+    private String generateRandomInput() {
+        String input = "";
+        
+        for(int i = 0; i < 32; i++) {
+            int base = (int) (Math.random() * 16);
+            switch(base) {
+                case 10:
+                    input += 'A';
+                    break;
+                case 11:
+                    input += 'B';
+                    break;
+                case 12:
+                    input += 'C';
+                    break;
+                case 13:
+                    input += 'D';
+                    break;
+                case 14:
+                    input += 'E';
+                    break;
+                case 15:
+                    input += 'F';
+                    break;
+                case 16: 
+                    input += 'F';
+                    break;
+                default:
+                    input += base;
+                    break;
+            }
+        }
+        return input;
+    }
+    
+    public double getAverage(Integer[] input) {
+        int sum = 0;
+        for(int i = 0; i < input.length; i++) {
+            sum += input[i];
+        }
+        
+        return (double) sum / input.length;
+    }
+    
+    public double getCorrelationCoefficient(Integer[] x, Integer[] y) {
+        double avgX = getAverage(x);
+        double avgY = getAverage(y);
+        
+        if(x.length != y.length)
+            return 0.0;
+        
+        int index = x.length;
+        double covX = 0;
+        double covY = 0;
+        double diffXY = 0;
+        for(int i = 0; i < index; i++) {
+            double tempX = x[i] - avgX;
+            double tempY = y[i] - avgY;
+            covX += Math.pow(tempX, 2);
+            covY += Math.pow(tempY, 2);
+            diffXY += (tempX * tempY);
+        }
+        
+        double divider = Math.sqrt(covX) * Math.sqrt(covY);
+        return diffXY / divider;
+    }
+    
+    //TODO move this to common?
+    private BitSet transformFromHex(String hex) {
+        return BitSet.valueOf(transformFromHexToByte(hex));
+    }
+    
+    private byte[] transformFromHexToByte(String hex) {
+        return DatatypeConverter.parseHexBinary(hex);
+    }
+    
+    private String transformFromBitSet(java.util.BitSet bs) {
+        return Long.toHexString(bs.toLongArray()[0]);
+    }
+    
+    private class Data {
+        List<Integer> xData = new ArrayList<Integer>();
+        List<Integer> yData = new ArrayList<Integer>();
+        
+        public void add(int x, int y) {
+            xData.add(x);
+            yData.add(y);
+        }
+        
+        public Integer[] getXData() {
+            return xData.toArray(new Integer[xData.size()]);
+        }
+        
+        public Integer[] getYData() {
+            return yData.toArray(new Integer[yData.size()]);
+        }
+        
+        @Override
+        public String toString() {
+            String result = "";
+            for(int i = 0; i < xData.size(); i++) {
+                result += ("" + xData.get(i) + "|" + yData.get(i) + "\n");
+            }
+            return result;
+        }
+    }
+    
+    public static byte[] toByteArray(BitSet bits) {
+        byte[] bytes = new byte[(bits.size() + 7) / 8];
+        for (int i=0; i<bits.length(); i++) {
+            if (bits.get(i)) {
+                bytes[bytes.length-i/8-1] |= 1<<(i%8);
+            }
+        }
+        return bytes;
+    }
+}
--- TestSuite/src/sac/SAC.java
+++ TestSuite/src/sac/SAC.java
@@ -22,40 +22,105 @@ public class SAC {
     private BitSet xor_result;
     private BlockCipherFactory cipherFactory;
     private byte[] plainText;
-    private int N = 128;
-    private int[][] sac_matrix = new int[N][N];
     private String key = "0123456789ABCDEFFEDCBA9876543210";
+    
+    private int N = 128;
+    private final int RUN = 1048576;
+    private int[][] sac_matrix = new int[N][N];
+    
+    private int[] chi_table = {523857, 524158, 524417, 524718, 1048576};
+    private double[] prob_chi_table = {0.200224, 0.199937, 0.199677, 0.199937, 0.200224};
+    private int[] observed_freq = new int[5];
+    private int[] expected_freq = new int[5];
     
     public SAC() {
         byte[] key_byte = transformFromHex(key).toByteArray();
         cipherFactory = new BlockCipherFactory(key_byte);
     }
     
-    public BitSet[] runTest() {
-        input = transformFromHex("00000000000000000123456789ABCDEF");
-        plainText = input.toByteArray();
-        baseOutput = BitSet.valueOf(cipherFactory.getEncrypt(plainText, CipherType.AES));
-        
-        System.out.println("SAC Matrix init...");
-        printSACMatrix();
-        
-        for(int i = 0; i < N; i++) {
-            temp = BitSet.valueOf(input.toByteArray());
-            temp.flip(i);
+    public double runTest() {
+        String inputStr;
+        for(int r = 0; r < RUN; r++) {
+            inputStr = generateRandomInput();
+            input = transformFromHex(inputStr);
             
-            plainText = temp.toByteArray();
-            output = BitSet.valueOf(cipherFactory.getEncrypt(plainText, CipherType.AES));
-            output.xor(baseOutput);
-            
-            for(int j = 0; j < N; j++) {
-                if(output.get(j))
-                    sac_matrix[i][j]++;
+            plainText = toByteArray(input);
+            baseOutput = BitSet.valueOf(cipherFactory.getEncrypt(plainText, CipherType.AES));
+  
+            for(int i = 0; i < N; i++) {
+                temp = BitSet.valueOf(input.toByteArray());
+                temp.flip(i);
+
+                plainText = toByteArray(temp);
+                output = BitSet.valueOf(cipherFactory.getEncrypt(plainText, CipherType.AES));
+                output.xor(baseOutput);
+
+                for(int j = 0; j < N; j++) {
+                    if(output.get(j))
+                        sac_matrix[i][j]++;
+                }
             }
             
-            System.out.println("Perubahan bit ke " + i);
+            System.out.println(r);
             printSACMatrix();
         }
-        return null;
+        
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                for(int k = 0; k < 5; k++) {
+                    if(sac_matrix[i][j] <= chi_table[k]) {
+                        observed_freq[k]++;
+                    }
+                }
+            }
+        }
+        
+        for(int i = 0; i < 5; i++) {
+            expected_freq[i] = (int) (prob_chi_table[i] * N * N);
+        }
+        
+        double cs = getChiSquare(5, observed_freq, expected_freq);
+        return cs;
+    }
+    
+    private byte[] generateRandomInputNew() {
+        byte[] result = new byte[16];
+        for(int i = 0; i < 16; i++) {
+            result[i] = (byte) (Math.random() * 256);
+        }
+        return result;
+    }
+    
+    private String generateRandomInput() {
+        String input = "";
+        
+        for(int i = 0; i < 32; i++) {
+            int base = (int) (Math.random() * 15);
+            switch(base) {
+                case 10:
+                    input += 'A';
+                    break;
+                case 11:
+                    input += 'B';
+                    break;
+                case 12:
+                    input += 'C';
+                    break;
+                case 13:
+                    input += 'D';
+                    break;
+                case 14:
+                    input += 'E';
+                    break;
+                case 15:
+                    input += 'F';
+                    break;
+                default:
+                    input += base;
+                    break;
+            }
+        }
+        return input;
     }
     
     private void printSACMatrix()
@@ -66,6 +131,15 @@ public class SAC {
             }
             System.out.println();
         }
+    }
+    
+    private double getChiSquare(int i, int[] observed, int[] expected) {
+        double cs = 0;
+        for(int j = 0; j < i; j++) {
+            int temp = observed[j] - expected[j];
+            cs += Math.pow(cs, 2) / expected[j];
+        }
+        return cs;
     }
     
     private BitSet transformFromHex(String hex) {
@@ -83,5 +157,15 @@ public class SAC {
     public static void main(String ar[]) {
         SAC sac = new SAC();
         sac.runTest();
+    }
+    
+    public static byte[] toByteArray(BitSet bits) {
+        byte[] bytes = new byte[(bits.size() + 7) / 8];
+        for (int i=0; i<bits.length(); i++) {
+            if (bits.get(i)) {
+                bytes[bytes.length-i/8-1] |= 1<<(i%8);
+            }
+        }
+        return bytes;
     }
 }
